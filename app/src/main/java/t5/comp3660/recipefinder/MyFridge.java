4d375612e1187e89126d38383c3051d930635d1a
jamessalvatore <jsalvatore171@gmail.com>
2018-06-13 19:12:18 -0400

    Initial app setup.

---
+++ app/src/main/java/t5/comp3660/recipefinder/MyFridge.java
@@ -0,0 +1,13 @@
+package t5.comp3660.recipefinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFridge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_fridge);
+    }
+}
