84e9a661146e79178c7152354eefbde08e58e224
jamessalvatore <jsalvatore171@gmail.com>
2018-06-13 19:17:47 -0400

    Dev branch creation.

--- app/src/main/java/t5/comp3660/recipefinder/MyFridge.java
+++ app/src/main/java/t5/comp3660/recipefinder/MyFridge.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MyFridge extends AppCompatActivity {
 
+    // Comment yo.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
