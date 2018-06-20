5fe0c6b7385d80bb7fa9d2186f508e9bf611bc47
jamessalvatore <jsalvatore171@gmail.com>
2018-06-19 20:51:10 -0400

    Added basic navigation to the myfridge and recipesearch fragments. Added all empty fragments.

---
+++ app/src/main/java/t5/comp3660/recipefinder/RecipeSearchFragment.java
@@ -0,0 +1,29 @@
+package t5.comp3660.recipefinder;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RecipeSearchFragment extends Fragment {
+
+
+    public RecipeSearchFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_search, container, false);
+    }
+
+}
